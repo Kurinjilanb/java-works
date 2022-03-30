ad5d4d9fcbd5e21f8d14cf0fc6257ae6e51435f9
Kurinjilanb <kurinjilan@gmail.com>
2022-03-30 11:28:08 +0530

    Updated my code

---
+++ Compare.java
@@ -0,0 +1,11 @@
+//given number is prime numbers.
+import java.util.*;
+public class Compare{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+if(n%2==1)
+System.out.println("Odd number");
+System.out.println("Even number");
+}
+}
